e0ff4c4773fbdf1877af4866b6d7215d302d00e5
Emiliano Barbosa <emiliano.barbosa@corp.globo.com>
2015-04-23 15:42:01 -0300

    Release 1.0.4

---
+++ EEBase.h
@@ -0,0 +1,15 @@
+//
+//  EEBase.h
+//  eloiAppBase
+//
+//  Created by Emiliano Barbosa on 4/23/15.
+//  Copyright (c) 2015 Emiliano Barbosa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EEBase : NSObject
+
+-(int)sum:(int)a andB:(int)b;
+
+@end
---
+++ EEBase.m
@@ -0,0 +1,17 @@
+//
+//  EEBase.m
+//  eloiAppBase
+//
+//  Created by Emiliano Barbosa on 4/23/15.
+//  Copyright (c) 2015 Emiliano Barbosa. All rights reserved.
+//
+
+#import "EEBase.h"
+
+@implementation EEBase
+
+-(int)sum:(int)a andB:(int)b{
+    return a + b;
+}
+
+@end
